a57da16a9948504638a949c16ce7a12dcc56dd7a
Tudor Lechintan <lechintantudor02@gmail.com>
2020-07-02 15:38:38 +0300

    Temporarily disable some lints

--- amethyst_ui/src/lib.rs
+++ amethyst_ui/src/lib.rs
@@ -1,4 +1,7 @@
 //! Provides components and systems to create an in game user interface.
+#![allow(dead_code)]
+#![allow(unreachable_code)]
+#![allow(unused_variables)]
 
 pub use self::{
     bundle::UiBundle,

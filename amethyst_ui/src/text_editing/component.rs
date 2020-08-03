5cfece3fe47765d7af911d57277464294f3e4b38
Tudor Lechintan <lechintantudor02@gmail.com>
2020-08-03 22:12:24 +0300

    Write documentation for text editing

--- amethyst_ui/src/text_editing/component.rs
+++ amethyst_ui/src/text_editing/component.rs
@@ -1,17 +1,27 @@
 use amethyst_rendy::palette::Srgba;
 
+// Enables text editing when attached to an entity with
+// a `UiText` component
 #[derive(Copy, Clone, Debug)]
 pub struct TextEditing {
+    /// Cursor position in graphemes
     pub cursor_position: isize,
+    /// Max number of graphemes in `UiText`'s string
     pub max_length: usize,
+    /// The highlight position in graphemes relative to `cursor_position`
     pub highlight_vector: isize,
+    /// The color of text when highlighted
     pub selected_text_color: Srgba,
+    /// The background color of text when highlighted
     pub selected_background_color: Srgba,
+    /// Whether to use a block cursor for editing. Not recommended in font
+    /// is not monospace
     pub use_block_cursor: bool,
     pub(crate) cursor_blink_timer: f32,
 }
 
 impl TextEditing {
+    /// Creates a new `TextEditing`.
     pub fn new(
         max_length: usize,
         selected_text_color: Srgba,
--- amethyst_ui/src/text_editing/utils.rs
+++ amethyst_ui/src/text_editing/utils.rs
@@ -2,10 +2,12 @@ use crate::{TextEditing, UiText};
 use std::ops::Range;
 use unicode_segmentation::UnicodeSegmentation;
 
+/// Returns the byte index of the cursor in `ui_text`'s string.
 pub fn cursor_byte_index(text_editing: &TextEditing, ui_text: &UiText) -> usize {
     cursor_byte_index_str(text_editing, &ui_text.text)
 }
 
+/// Returns the byte index of the cursor in `text`.
 pub fn cursor_byte_index_str(text_editing: &TextEditing, text: &str) -> usize {
     text.grapheme_indices(true)
         .nth(text_editing.cursor_position.max(0) as usize)
@@ -13,10 +15,12 @@ pub fn cursor_byte_index_str(text_editing: &TextEditing, text: &str) -> usize {
         .unwrap_or(text.len())
 }
 
+/// Returns the range of highlighted bytes in `ui_text`'s string.
 pub fn highlighted_bytes_range(text_editing: &TextEditing, ui_text: &UiText) -> Range<usize> {
     highlighted_bytes_range_str(text_editing, &ui_text.text)
 }
 
+/// Returns the range of highlighted bytes in `text`.
 pub fn highlighted_bytes_range_str(text_editing: &TextEditing, text: &str) -> Range<usize> {
     let start = text_editing
         .cursor_position
@@ -37,18 +41,22 @@ pub fn highlighted_bytes_range_str(text_editing: &TextEditing, text: &str) -> Ra
     start_byte..end_byte
 }
 
+/// Returns the highlighted text as a `str`.
 pub fn highlighted_text<'a>(text_editing: &TextEditing, ui_text: &'a UiText) -> &'a str {
     highlighted_text_str(text_editing, &ui_text.text)
 }
 
+/// Returns the highlighted text as a `str`.
 pub fn highlighted_text_str<'a>(text_editing: &TextEditing, text: &'a str) -> &'a str {
     &text[highlighted_bytes_range_str(text_editing, text)]
 }
 
+/// Deletes the highlighted text and returns `true` if anything was deleted or `false` otherwise.
 pub fn delete_highlighted_text(text_editing: &mut TextEditing, ui_text: &mut UiText) -> bool {
     delete_highlighted_text_string(text_editing, &mut ui_text.text)
 }
 
+/// Deletes the highlighted text and returns `true` if anything was deleted or `false` otherwise.
 pub fn delete_highlighted_text_string(text_editing: &mut TextEditing, text: &mut String) -> bool {
     if text_editing.highlight_vector == 0 {
         return false;
@@ -62,10 +70,12 @@ pub fn delete_highlighted_text_string(text_editing: &mut TextEditing, text: &mut
     true
 }
 
+/// Deletes the highlighted text from `ui_text` and returns it.
 pub fn extract_highlighted_text(text_editing: &mut TextEditing, ui_text: &mut UiText) -> String {
     extract_highlighted_text_string(text_editing, &mut ui_text.text)
 }
 
+/// Deletes the highlighted text from `text` and returns it.
 pub fn extract_highlighted_text_string(
     text_editing: &mut TextEditing,
     text: &mut String,
